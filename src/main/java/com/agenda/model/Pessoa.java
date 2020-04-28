499f6d9ec68a57d4c37ec26083b93dab2f05422f
Enoque Leal <enoquefelipe@gmail.com>
2020-04-27 22:54:51 -0300

    ajuste na anotação @GeneratedValue

--- src/main/java/com/agenda/model/Pessoa.java
+++ src/main/java/com/agenda/model/Pessoa.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
@@ -17,7 +18,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 public class Pessoa {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 
