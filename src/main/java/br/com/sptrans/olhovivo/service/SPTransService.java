08550c838c1a968747f365eeeab30bd20104dc3c
Enoque Leal <enoquefelipe@gmail.com>
2020-07-26 06:20:58 -0300

    first adjust in service class

--- src/main/java/br/com/sptrans/olhovivo/service/SPTransService.java
+++ src/main/java/br/com/sptrans/olhovivo/service/SPTransService.java
@@ -21,50 +21,37 @@ public class SPTransService {
 	public SPTransService(EnvironmentConfiguration env, RestTemplateBuilder builder) {
 		this.env = env;
 		this.restTemplate = builder.build();
+		this.httpEntity = getHttpEntity();
 	}
 
+	private HttpEntity<String> getHttpEntity() {
+
+		String uri = env.getUrl().concat("Login/Autenticar?token=").concat(env.getToken());
+
+		ResponseEntity<String> responseEntity = restTemplate.postForEntity(uri, null, String.class);
+
+		return new HttpEntity<String>(responseEntity.getHeaders());
+
+	}
+	
 	public List<Line> getLines(String parameter) {
 		
-		httpEntity = getHttpEntity();
-
-		String uri = env.getUrl() + "Linha/Buscar?termosBusca=" + parameter;
+		String uri = env.getUrl().concat("Linha/Buscar?termosBusca=").concat(parameter);
 		
 		ResponseEntity<List<Line>> responseEntity = restTemplate.exchange(uri, HttpMethod.GET, httpEntity, new ParameterizedTypeReference<List<Line>>() {});
 
 		return responseEntity.getBody();
 
 	}
-	
+
 	public Location getLocation(Integer parameter){
 		
-		httpEntity = getHttpEntity();
-
-		String uri = env.getUrl() + "Posicao/Linha?codigoLinha=" + parameter;
+		String uri = env.getUrl().concat("Posicao/Linha?codigoLinha=").concat(String.valueOf(parameter));
 		
 		ResponseEntity<Location> responseEntity = restTemplate.exchange(uri, HttpMethod.GET, httpEntity, new ParameterizedTypeReference<Location>() {});
 		
 		return responseEntity.getBody();
 		
-	}
-
-	private HttpEntity<String> getHttpEntity() {
-
-		String uri = env.getUrl() + "Login/Autenticar?token=" + env.getToken();
-
-		ResponseEntity<String> responseEntity = restTemplate.postForEntity(uri, null, String.class);
-
-		List<String> headersItens = responseEntity.getHeaders().get("Set-Cookie");
-
-		HttpHeaders headers = new HttpHeaders();
-
-		for (String item : headersItens) {
-			String cookieValue = item.substring(15, 407);
-			headers.set("apiCredentials", cookieValue);
-			break;
-		}
-
-		return new HttpEntity<String>(headers);
-
 	}
 
 }
